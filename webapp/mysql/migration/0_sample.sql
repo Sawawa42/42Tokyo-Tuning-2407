d5945e0b7513989a5eeae5761eaaa9c663b34034
syamasaw <17191sawa@gmail.com>
2024-07-28 01:55:38 +0900

    8

--- webapp/mysql/migration/0_sample.sql
+++ webapp/mysql/migration/0_sample.sql
@@ -1,3 +1,5 @@
 -- このファイルに記述されたSQLコマンドが、マイグレーション時に実行されます。
+set global slow_query_log=1;
+set global long_query_time=0.1;
 CREATE INDEX idx_tow_truck_id_timestamp ON locations (tow_truck_id, timestamp);
-CREATE INDEX idx_username ON users (username);
+-- CREATE INDEX idx_username ON users (username);
