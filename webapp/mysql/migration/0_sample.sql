a039254bc494e1da2b8ffcce2d3ab0bb4992e502
syamasaw <17191sawa@gmail.com>
2024-07-28 10:43:17 +0900

    username index

--- webapp/mysql/migration/0_sample.sql
+++ webapp/mysql/migration/0_sample.sql
@@ -2,4 +2,4 @@
 set global slow_query_log=1;
 set global long_query_time=0.1;
 CREATE INDEX idx_tow_truck_id_timestamp ON locations (tow_truck_id, timestamp);
--- CREATE INDEX idx_username ON users (username);
+CREATE INDEX idx_username ON users (username);
